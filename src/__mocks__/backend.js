24076108c015f98c3f713dd03887b58ac7699e33
Pieter Wigboldus <w3n@w3news.org>
2020-11-13 16:19:13 +0100

    Initial commit

---
+++ src/__mocks__/backend.js
@@ -0,0 +1,22 @@
+export default class Backend {
+  constructor () {
+    this.routes = []
+        this.security = []
+    }
+
+  register (operationId, handler) {
+    this.routes.push({
+      operationId,
+      handler
+    })
+    }
+
+  registerSecurityHandler (name, handler) {
+    this.security.push({
+      name,
+      handler
+    })
+    }
+
+  init () {}
+}
---
+++ src/__tests__/api.feature.js
@@ -0,0 +1,57 @@
+import { ApiRoutes } from '../api.js'
+import OpenAPISpecification from '../__fixtures__/api-doc.json'
+import Backend from '../__mocks__/backend.js'
+import callback from '../__mocks__/callback.js'
+import controllers from '../__mocks__/controller.js'
+
+describe('API route without security', () => {
+  const actual = ApiRoutes.create({
+    specification: OpenAPISpecification,
+    Backend,
+    controllers,
+    callback
+  })
+    it('Test if the return value is a instance of the backend', () => {
+    expect(actual instanceof Backend).toBeTruthy()
+    })
+
+    it('Test if the API route add all routes', () => {
+    expect(actual.routes.map((route) => route.operationId)).toEqual([
+      'getStatus',
+      'getDiscounts',
+      'getDiscount',
+      'notFound'
+    ])
+    })
+
+    it('Test if the API security is empty', () => {
+    expect(actual.security.map((route) => route.name)).toEqual([])
+    })
+})
+
+describe('API route with security', () => {
+  const actual = ApiRoutes.create({
+    specification: OpenAPISpecification,
+    secret: 'secret',
+    Backend,
+    controllers,
+    callback
+  })
+    it('Test if the return value is a instance of the backend', () => {
+    expect(actual instanceof Backend).toBeTruthy()
+    })
+
+    it('Test if the API route add all routes', () => {
+    expect(actual.routes.map((route) => route.operationId)).toEqual([
+      'unauthorizedHandler',
+      'getStatus',
+      'getDiscounts',
+      'getDiscount',
+      'notFound'
+    ])
+    })
+
+    it('Test if the API security is set', () => {
+    expect(actual.security.map((route) => route.name)).toEqual(['apiKey'])
+    })
+})
